59f54baa39ca6520d9d15294e0232a44e33a1a91
KarolMic <karolmicki@gmail.com>
2024-04-23 14:34:53 +0200

    config, eureka

---
+++ src/main/java/pl/polregio/pkm_trains/PkmTrainsApplication.java
@@ -0,0 +1,12 @@
+package pl.polregio.pkm_trains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkmTrainsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkmTrainsApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/polregio/pkm_trains/PkmTrainsApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.polregio.pkm_trains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkmTrainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
